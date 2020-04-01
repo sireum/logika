95a04c896ea01142204dce27570bdd82535d2065
Robby <robby@k-state.edu>
2020-04-01 13:26:51 -0500

    Added C and Z bit-width options.

--- jvm/src/test/scala/org/sireum/logika/LogikaRcTest.scala
+++ jvm/src/test/scala/org/sireum/logika/LogikaRcTest.scala
@@ -39,7 +39,7 @@ class LogikaRcTest extends SireumRcSpec {
   def check(path: scala.Vector[Predef.String], content: Predef.String): scala.Boolean = {
     val reporter = Reporter.create
     Logika.checkWorksheet(Some(Os.path(path.mkString(Os.fileSep.value)).string), content, config,
-      th => Z3(z3Exe, th), reporter)
+      th => Z3(z3Exe, th, config.charBitWidth, config.intBitWidth), reporter)
     reporter.printMessages()
     !reporter.hasIssue
   }
