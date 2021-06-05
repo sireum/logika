8475dbee933d5bfc7b592749e297968350fa0adf
Robby <robby@santoslab.org>
2021-06-05 15:52:03 -0500

    Updated z3 download url.

--- bin/build.cmd
+++ bin/build.cmd
@@ -104,7 +104,7 @@ def installZ3(): Unit = {
 
   val filename: String = Os.kind match {
     case Os.Kind.Win => s"z3-$version-x64-win.zip"
-    case Os.Kind.Linux => s"z3-$version-x64-ubuntu-18.04.zip"
+    case Os.Kind.Linux => s"z3-$version-x64-glibc-2.31.zip"
     case Os.Kind.Mac => s"z3-$version-x64-osx-10.15.7.zip"
     case _ => return
   }
