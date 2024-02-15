a92672ff6ae6166c63611dbaf267b946f34a04c4
Robby <robby@santoslab.org>
2024-02-15 12:24:40 -0600

    Rewriting system.

--- jvm/src/test/scala/org/sireum/logika/example/rewrite.sc
+++ jvm/src/test/scala/org/sireum/logika/example/rewrite.sc
@@ -13,29 +13,98 @@ object Rules {
   )
 
   @rw val myRewriteSet = RS(zDistribute _)
+}
 
-  @pure def zDistributeTest(c: Z, d: Z): Unit = {
-    Deduce(
-      (2 * c + 3 * c == d) |- (5 * c == d) Proof(
-        //@formatter:off
-        1  (2 * c + 3 * c == d)  by Premise,
-        2  (5 * c == d)          by Rewrite(RS(zDistribute _), 1) T,
-        3  (5 * c == d)          by Rewrite(myRewriteSet, 1) T
-        //@formatter:on
-      )
-    )
-  }
+import Rules._
 
-  @pure def substTest(c: Z, d: Z): Unit = {
-    Deduce(
-      (c ≡ d, (c + 1) ≡ 3) |- ((d + 1) ≡ 3) Proof(
-        //@formatter:off
-        1  (c ≡ d)              by Premise,
-        2  ((c + 1) ≡ 3)        by Premise,
-        3  ((d + 1) ≡ 3)        by Rewrite(RS(subst _), 2),
-        4  ((c + 1) ≡ 3)        by Rewrite(~RS(subst _), 3) and (1, 3) // and (...) is optional but it makes the search faster
-        //@formatter:on
-      )
+@pure def zDistributeTest(c: Z, d: Z): Unit = {
+  Deduce(
+    (2 * c + 3 * c == d) |- (5 * c == d) Proof(
+      //@formatter:off
+      1  (2 * c + 3 * c == d)  by Premise,
+      2  (5 * c == d)          by Rewrite(RS(zDistribute _), 1) T,
+      3  (5 * c == d)          by Rewrite(myRewriteSet, 1) T
+      //@formatter:on
     )
-  }
+  )
+}
+
+@pure def substTest(c: Z, d: Z): Unit = {
+  Deduce(
+    (c ≡ d, (c + 1) ≡ 3) |- ((d + 1) ≡ 3) Proof(
+      //@formatter:off
+      1  (c ≡ d)              by Premise,
+      2  ((c + 1) ≡ 3)        by Premise,
+      3  ((d + 1) ≡ 3)        by Rewrite(RS(subst _), 2),
+      //@formatter:on
+    )
+  )
+}
+
+@pure def substTestR(c: Z, d: Z): Unit = {
+  Deduce(
+    (c ≡ d, (d + 1) ≡ 3) |- ((c + 1) ≡ 3) Proof(
+      //@formatter:off
+      1  (c ≡ d)              by Premise,
+      2  ((d + 1) ≡ 3)        by Premise,
+      4  ((c + 1) ≡ 3)        by Rewrite(~RS(subst _), 2) and (1, 2) // and (...) is optional but it makes the search faster
+      //@formatter:on
+    )
+  )
+}
+
+@datatype class EvalFoo[X, Y](val x: X, val y: Y)
+
+@pure def binaryConstantPropagation(): Unit = {
+  Deduce(
+    //@formatter:off
+    1  (1 + 2 == 3)  by Simpl
+    //@formatter:on
+  )
+}
+
+@pure def unaryConstantPropagation(): Unit = {
+  Deduce(
+    //@formatter:off
+    1  (-(-1) == 1)  by Simpl
+    //@formatter:on
+  )
+}
+
+@pure def fieldAccess(o: EvalFoo[Z, Z]): Unit = {
+  Deduce(
+    //@formatter:off
+    1  (EvalFoo(x = 4, y = 5).x == 4)   by Simpl,
+    3  (o(x = 4, y = 5).x == 4)         by Simpl,
+    5  (o(x = 4, y = 5).y == 5)         by Simpl,
+    7  (o(x = 4)(y = 1)(x = 5).x == 5)  by Simpl,
+    9  (o(x = 4)(x = 5).y == o.y)       by Simpl
+    //@formatter:on
+  )
+}
+
+@pure def tupleProjection(): Unit = {
+  Deduce(
+    //@formatter:off
+    1  ((1, 2)._1 == 1)   by Simpl,
+    3  ((1, 2)._2 == 2)   by Simpl
+    //@formatter:on
+  )
+}
+
+@pure def indexing[T](a: ISZ[T], i: Z, j: Z, k: Z, t1: T, t2: T): Unit = {
+  Contract(
+    Requires(i != j, i == k, 0 <= i, i < a.size, 0 <= j, j < a.size)
+  )
+  Deduce(
+    //@formatter:off
+    1  (i != j)                             by Premise,
+    2  (i == k)                             by Premise,
+    3  (ISZ(1, 2, 3).size == 3)             by Simpl,
+    5  (ISZ[Z](1, 2, 3)(0 ~> 5).size == 3)  by Simpl,
+    7  (a(0 ~> t1).size == a.size)          by Simpl,
+    9  (a(i ~> t1)(j ~> t2)(i) == t1)       by Simpl,
+    11 (a(i ~> t1)(k ~> t2)(i) == t2)       by Simpl
+    //@formatter:on
+  )
 }
--- shared/src/main/scala/org/sireum/logika/plugin/RewritePlugin.scala
+++ shared/src/main/scala/org/sireum/logika/plugin/RewritePlugin.scala
@@ -40,9 +40,11 @@ import org.sireum.logika.{Logika, RewritingSystem, Smt2, State, StepProofContext
 
   override def canHandle(logika: Logika, just: AST.ProofAst.Step.Justification): B = {
     just match {
+      case just: AST.ProofAst.Step.Justification.Ref =>
+        return just.id.value == "Simpl" && just.isOwnedBy(justificationName)
       case just: AST.ProofAst.Step.Justification.Apply =>
         just.invoke.ident.attr.resOpt match {
-          case Some(res: AST.ResolvedInfo.Method) if (res.id == "Rewrite" || res.id == "Eval") && res.owner == justificationName => return T
+          case Some(res: AST.ResolvedInfo.Method) if res.id == "Rewrite" && res.owner == justificationName => return T
           case _ =>
         }
       case _ =>
@@ -58,30 +60,21 @@ import org.sireum.logika.{Logika, RewritingSystem, Smt2, State, StepProofContext
                       spcMap: HashSMap[AST.ProofAst.StepId, StepProofContext], state: State,
                       step: AST.ProofAst.Step.Regular, reporter: Logika.Reporter): Plugin.Result = {
     @strictpure def emptyResult: Plugin.Result = Plugin.Result(F, state.nextFresh, ISZ())
-    val just = step.just.asInstanceOf[AST.ProofAst.Step.Justification.Apply]
-    val isEval = just.id.value == "Eval"
+    @strictpure def justArgs: ISZ[AST.Exp] = step.just.asInstanceOf[AST.ProofAst.Step.Justification.Apply].args
+    val isSimpl = step.just.id.value == "Simpl"
     val patterns: ISZ[Rewriter.Pattern] =
-      if (isEval) ISZ()
-      else RewritingSystem.retrievePatterns(logika.th, cache, just.args(0))
-    val from: AST.ProofAst.StepId =
-      AST.Util.toStepIds(ISZ(just.args(if (isEval) 0 else 1)), Logika.kind, reporter) match {
-        case Some(s) => s(0)
-        case _ => return emptyResult
-      }
-    val fromClaim: AST.Exp = spcMap.get(from) match {
-      case Some(spc: StepProofContext.Regular) => spc.exp
-      case _ =>
-        reporter.error(from.posOpt, Logika.kind, s"Expecting a regular proof step")
-        return emptyResult
-    }
+      if (isSimpl) ISZ()
+      else RewritingSystem.retrievePatterns(logika.th, cache, justArgs(0))
     var provenClaims = HashSMap.empty[AST.ProofAst.StepId, AST.CoreExp.Base]
-    if (just.hasWitness) {
-      for (w <- just.witnesses) {
+    if (step.just.hasWitness) {
+      for (w <- step.just.witnesses) {
         spcMap.get(w) match {
           case Some(spc: StepProofContext.Regular) =>
             provenClaims = provenClaims + w ~> spc.coreExpClaim
+          case Some(_) =>
+            reporter.error(w.posOpt, Logika.kind, s"Expecting a regular proof step for $w")
           case _ =>
-            reporter.error(from.posOpt, Logika.kind, s"Expecting a regular proof step for $w")
+            reporter.error(w.posOpt, Logika.kind, s"Could not find proof step $w")
         }
       }
     } else {
@@ -93,71 +86,135 @@ import org.sireum.logika.{Logika, RewritingSystem, Smt2, State, StepProofContext
         }
       }
     }
-    val rwPc = Rewriter(if (logika.config.rwPar) logika.config.parCores else 1,
-      logika.th, provenClaims, patterns, logika.config.rwTrace, F, ISZ())
-    val fromCoreClaim = RewritingSystem.translate(logika.th, F, fromClaim)
-    var rwClaim = fromCoreClaim
-    val stepClaim = RewritingSystem.translate(logika.th, F, step.claim)
-    if (isEval) {
-      if (logika.config.rwTrace) {
-        rwPc.trace = rwPc.trace :+ RewritingSystem.Trace.Begin("evaluating", rwClaim)
-      }
-      rwClaim = RewritingSystem.evalBase(logika.th, RewritingSystem.EvalConfig.all,
-        rwPc.provenClaimStepIdMap, rwClaim, logika.config.rwTrace) match {
-        case Some((e, t)) =>
-          rwPc.trace = t :+ RewritingSystem.Trace.Done(rwClaim, e)
-          e
-        case _ => rwClaim
-      }
-    } else {
-      var done = F
-      var i = 0
-      while (!done && i < logika.config.rwMax && rwClaim != stepClaim) {
-        rwPc.done = F
-        if (logika.config.rwTrace) {
-          rwPc.trace = rwPc.trace :+ RewritingSystem.Trace.Begin("rewriting", rwClaim)
-        }
-        rwClaim = rwPc.transformCoreExpBase(rwClaim) match {
-          case MSome(c) =>
-            rwPc.trace = rwPc.trace :+ RewritingSystem.Trace.Done(rwClaim, c)
-            c
-          case _ =>
-            done = T
-            rwClaim
-        }
-        i = i + 1
-      }
-    }
-    val traceOpt: Option[ST] = if (logika.config.rwTrace) {
+    @strictpure def traceOpt(trace: ISZ[RewritingSystem.Trace]): Option[ST] = if (logika.config.rwTrace) {
       Some(
         st"""Trace:
             |
-            |${(for (te <- rwPc.trace) yield te.toST, "\n\n")}""")
+            |${(for (te <- trace) yield te.toST, "\n\n")}""")
     } else {
       None()
     }
+    val rwPc = Rewriter(if (logika.config.rwPar) logika.config.parCores else 1,
+      logika.th, provenClaims, patterns, logika.config.rwTrace, F, ISZ())
+    val stepClaim = RewritingSystem.translate(logika.th, F, step.claim)
+
+    if (logika.config.rwTrace) {
+      rwPc.trace = rwPc.trace :+ RewritingSystem.Trace.Begin("simplifying", stepClaim)
+    }
+
+    val stepClaimEv: AST.CoreExp.Base = RewritingSystem.evalBase(logika.th, RewritingSystem.EvalConfig.all,
+      rwPc.provenClaimStepIdMap, stepClaim, logika.config.rwTrace) match {
+      case Some((e, t)) =>
+        rwPc.trace = t
+        e
+      case _ => stepClaim
+    }
+
+    if (logika.config.rwTrace) {
+      rwPc.trace = rwPc.trace :+ RewritingSystem.Trace.Done(stepClaim, stepClaimEv)
+    }
+
+    if (stepClaimEv == AST.CoreExp.LitB(T)) {
+      reporter.inform(step.just.id.attr.posOpt.get, Reporter.Info.Kind.Verified,
+        st"""Evaluating ${stepClaim.prettyST} produces T, hence the claim holds
+            |
+            |${traceOpt(rwPc.trace)}""".render)
+      if (isSimpl) {
+        val q = logika.evalRegularStepClaimRtCheck(smt2, cache, F, state, step.claim, step.id.posOpt, reporter)
+        val (stat, nextFresh, claims) = (q._1, q._2, q._3 :+ q._4)
+        return Plugin.Result(stat, nextFresh, claims)
+      } else {
+        reporter.warn(step.just.id.attr.posOpt, Logika.kind, "The claim can be discharged by using Simpl instead")
+      }
+    } else if (isSimpl) {
+      reporter.error(step.just.id.attr.posOpt, Logika.kind,
+        st"""Could not simplify ${stepClaim.prettyST} to T
+            |
+            |After simplification:
+            |  ${stepClaimEv.prettyST}
+            |
+            |${traceOpt(rwPc.trace)}""".render)
+      return emptyResult
+    }
+
+    val simplTrace = rwPc.trace
+    rwPc.trace = ISZ()
+    val from: AST.ProofAst.StepId =
+      AST.Util.toStepIds(ISZ(justArgs(1)), Logika.kind, reporter) match {
+        case Some(s) => s(0)
+        case _ => return emptyResult
+      }
+    val fromClaim: AST.Exp = spcMap.get(from) match {
+      case Some(spc: StepProofContext.Regular) => spc.exp
+      case _ =>
+        reporter.error(from.posOpt, Logika.kind, s"Expecting a regular proof step")
+        return emptyResult
+    }
+    val fromCoreClaim = RewritingSystem.translate(logika.th, F, fromClaim)
+    var rwClaim = fromCoreClaim
+    var done = F
+    var i = 0
+    while (!done && i < logika.config.rwMax && rwClaim != stepClaim) {
+      rwPc.done = F
+      if (logika.config.rwTrace) {
+        rwPc.trace = rwPc.trace :+ RewritingSystem.Trace.Begin("rewriting", rwClaim)
+      }
+      rwClaim = rwPc.transformCoreExpBase(rwClaim) match {
+        case MSome(c) =>
+          rwPc.trace = rwPc.trace :+ RewritingSystem.Trace.Done(rwClaim, c)
+          c
+        case _ =>
+          done = T
+          rwClaim
+      }
+      i = i + 1
+    }
+    @strictpure def simplTraceOpt: Option[ST] = if (stepClaim == stepClaimEv) None() else Some(
+      st"""and/or after simplifying the step claim to:
+          |  ${stepClaimEv.prettyST}"""
+    )
     if (rwClaim == stepClaim) {
-      reporter.inform(just.id.attr.posOpt.get, Reporter.Info.Kind.Verified,
+      reporter.inform(step.just.id.attr.posOpt.get, Reporter.Info.Kind.Verified,
         st"""Matched:
             |  ${stepClaim.prettyST}
             |
-            |After ${if (isEval) "evaluating" else "rewriting"} $from:
+            |After rewriting $from:
             |  ${fromCoreClaim.prettyST}
             |
-            |$traceOpt""".render)
+            |${traceOpt(rwPc.trace)}""".render)
       val q = logika.evalRegularStepClaimRtCheck(smt2, cache, F, state, step.claim, step.id.posOpt, reporter)
       val (stat, nextFresh, claims) = (q._1, q._2, q._3 :+ q._4)
       return Plugin.Result(stat, nextFresh, claims)
-    } else {
-      reporter.error(just.id.attr.posOpt, Logika.kind,
-        st"""Could not match:
+    } else if (rwClaim == stepClaimEv) {
+      reporter.inform(step.just.id.attr.posOpt.get, Reporter.Info.Kind.Verified,
+        st"""Matched:
             |  ${stepClaim.prettyST}
             |
-            |After ${if (isEval) "evaluating" else "rewriting"} $from to:
-            |  ${rwClaim.prettyST}
+            |After rewriting $from:
+            |  ${fromCoreClaim.prettyST}
             |
-            |$traceOpt""".render)
-      return emptyResult
+            |$simplTraceOpt
+            |
+            |${traceOpt(rwPc.trace)}
+            |
+            |${traceOpt(simplTrace)}""".render)
+    } else {
+        reporter.error(step.just.id.attr.posOpt, Logika.kind,
+          st"""Could not match:
+              |  ${stepClaim.prettyST}
+              |
+              |After rewriting $from to:
+              |  ${rwClaim.prettyST}
+              |
+              |$simplTraceOpt
+              |
+              |${traceOpt(rwPc.trace)}
+              |
+              |${traceOpt(simplTrace)}""".render)
+        return emptyResult
     }
+    val q = logika.evalRegularStepClaimRtCheck(smt2, cache, F, state, step.claim, step.id.posOpt, reporter)
+    val (stat, nextFresh, claims) = (q._1, q._2, q._3 :+ q._4)
+    return Plugin.Result(stat, nextFresh, claims)
   }
 }
